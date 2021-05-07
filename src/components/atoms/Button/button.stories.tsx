62b6b8f73f2431337d9b915abcc583a84ff42e16
linjiansi <pmon.ken.1467@gmail.com>
2021-05-07 20:03:25 +0900

    Configure a button component

---
+++ src/components/atoms/Button/button.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './index';
+
+export default {
+    title: 'Button',
+    component: Button,
+};
+
+export function ContainedButton() {
+    return <Button></Button>;
+}
