d0404f2a5ac8eaa0cf42e2db7a526e7afa555083
linjiansi <pmon.ken.1467@gmail.com>
2021-05-10 15:49:11 +0900

    Add addon-knobs to text-field story

--- src/components/atoms/TextField/textfield.stories.tsx
+++ src/components/atoms/TextField/textfield.stories.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Field from './index';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
     title: 'TextField',
     component: Field,
+    decorators: [withKnobs],
 };
 
 export function TextField() {
-    return <Field></Field>;
+    const textFieldLabel = text('text', 'フィールド');
+    return <Field>{textFieldLabel}</Field>;
 }
-
