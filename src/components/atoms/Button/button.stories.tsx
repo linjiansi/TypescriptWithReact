119a5c1d1520e6f99de2b03e963504d819275bde
linjiansi <pmon.ken.1467@gmail.com>
2021-05-10 15:47:43 +0900

    Add addon-knobs to button story

--- src/components/atoms/Button/button.stories.tsx
+++ src/components/atoms/Button/button.stories.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Button from './index';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
     title: 'Button',
     component: Button,
+    decorators: [withKnobs],
 };
 
 export function ContainedButton() {
-    return <Button></Button>;
+    const buttonLabel = text('text', 'ボタン');
+    return <Button>{buttonLabel}</Button>;
 }
