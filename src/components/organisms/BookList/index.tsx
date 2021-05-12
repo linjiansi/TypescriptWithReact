86e823c78db823ac2a4cb30d3a37248c360d9f37
linjiansi <pmon.ken.1467@gmail.com>
2021-05-12 16:30:18 +0900

    Configure book list component

---
+++ src/components/organisms/BookList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Header from '../../molecules/Header/index';
+import BookCard from '../../molecules/BookCard/index';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+export default function BookList() {
+    return (
+        <>
+            <Header title={'書籍一覧'} buttonTitle={'CREATE'}></Header>
+            <Grid container spacing={4}>
+                {cards.map((card) => (
+                    <Grid item key={card} xs={12} sm={6} md={4}>
+                        <BookCard bookName={'hogehoge'}></BookCard>
+                    </Grid>
+                ))}
+            </Grid>
+        </>
+    );
+}
