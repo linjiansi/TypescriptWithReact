3601ad2474d26f3e570e2b7b3e185fb5c35a79d2
khayashi-cq <k.hayashi@caraquri.com>
2021-05-12 16:30:18 +0900

    Configure book list component

---
+++ src/components/organisms/BookList/bookList.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BookList from './index';
+
+export default {
+    title: 'BookList',
+    component: BookList,
+};
+
+export function story() {
+    return <BookList></BookList>
+}
