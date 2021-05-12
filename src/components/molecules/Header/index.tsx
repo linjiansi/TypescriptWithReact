d64942e2d3093508912bd1aa07f040b51410dc50
khayashi-cq <k.hayashi@caraquri.com>
2021-05-12 15:24:24 +0900

    Add props to header component

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -5,6 +5,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+type Props = {
+    title: string,
+    buttonTitle: string,
+};
+
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
@@ -16,14 +21,15 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
-export default function Header() {
+export default function Header(props: Props) {
+    const { title, buttonTitle } = props;
     const classes = useStyles();
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title}>Book Manager</Typography>
-                    <Button>Logout</Button>
+                    <Typography variant="h6" className={classes.title}>{title}</Typography>
+                    <Button>{buttonTitle}</Button>
                 </Toolbar>
             </AppBar>
         </div>
