8214e6ee22da76a0bb8ed180d10166322eb6728c
linjiansi <pmon.ken.1467@gmail.com>
2021-05-12 14:24:51 +0900

    Configure input book component

---
+++ src/components/molecules/InputBook/InputBook.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import InputBook from './index';
+
+export default {
+    title: 'InputBook',
+    component: InputBook,
+}
+
+export function story() {
+    return <InputBook></InputBook>
+}
