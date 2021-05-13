c8dfd55f362419cc2c5aad092ae310c30591d380
linjiansi <pmon.ken.1467@gmail.com>
2021-05-13 15:32:48 +0900

    Adjust layout for input book component

--- src/components/molecules/InputBook/index.tsx
+++ src/components/molecules/InputBook/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import TextField from '../../atoms/TextField/index';
+import styled from 'styled-components';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+
+const TextFieldContainer = styled.div`
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    background-color: yellow;
+    margin: 20px;
+`;
+
+const Img = styled.img`
+    width: 16rem;
+    height: 16rem;
+`;
+
+const InputBookContainer = styled.div`
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+    background-color: pink;
+    margin: 50px 20px;
+`;
 
 export default function InputBook() {
     return (
-        <div>
-            <img src='https://source.unsplash.com/random'></img>
-            <TextField inputType={'bookName'}></TextField>
-            <TextField inputType={'bookPrice'}></TextField>
-            <TextField inputType={'purchaseDate'}></TextField>
-        </div>
+        <InputBookContainer>
+            <Img src='https://source.unsplash.com/random'></Img>
+            <TextFieldContainer>
+                <TextField inputType={'bookName'}></TextField>
+                <TextField inputType={'bookPrice'}></TextField>
+                <TextField inputType={'purchaseDate'}></TextField>
+            </TextFieldContainer>
+        </InputBookContainer>
     );
 }
 
