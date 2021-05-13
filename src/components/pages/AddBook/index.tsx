83188bf5d23296bb2625d5a62735a9d78a2601da
khayashi-cq <k.hayashi@caraquri.com>
2021-05-13 18:27:40 +0900

    Configure add book page component

---
+++ src/components/pages/AddBook/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from '../../molecules/Header/index';
+import BookScreen from '../../organisms/BookScreen/index';
+import Paper from '@material-ui/core/Paper';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        paper: {
+            margin: theme.spacing(8),
+        },
+    }),
+);
+
+
+export default function AddBook() {
+    const classes = useStyles();
+
+    return (
+        <>
+            <Header headerType={'main'}></Header>
+            <Paper elevation={8} className={classes.paper}>
+                <BookScreen bookMode='add'></BookScreen>
+            </Paper>
+        </>
+    );
+}
