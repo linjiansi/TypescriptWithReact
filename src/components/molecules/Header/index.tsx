76525ec528dcc745065f4ce723ea41f7f9f9c150
linjiansi <pmon.ken.1467@gmail.com>
2021-05-13 10:36:40 +0900

    Modify header component to add header type props

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Button from '../../atoms/Button/index';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { primaryColor, whiteColor } from '../../../utils/color';
 
 type Props = {
-    title: string,
-    buttonTitle: string,
+    headerType: HeaderType,
 };
+
+export type HeaderType = 'main' |
+                  'bookList' |
+                  'editBook' |
+                  'addBook';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -21,17 +26,68 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
+const returnHeaderTitle = (headerType: HeaderType) => {
+    switch (headerType) {
+        case 'main':
+            return 'BookManager';
+            break;
+        case 'bookList':
+            return '書籍一覧';
+            break;
+        case 'addBook':
+            return '書籍追加';
+            break;
+        case 'editBook':
+            return '書籍編集';
+            break;
+    }
+};
+
+const returnButtonTitle = (headerType: HeaderType) => {
+    switch (headerType) {
+        case 'main':
+            return 'LOGOUT';
+            break;
+        case 'bookList':
+            return 'CREATE';
+            break;
+        case 'addBook':
+            return 'CREATE';
+            break;
+        case 'editBook':
+            return 'UPDATE';
+            break;
+    }
+};
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: primaryColor(),
+        },
+        secondary: {
+            main: whiteColor(),
+        },
+    },
+});
+
+
 export default function Header(props: Props) {
-    const { title, buttonTitle } = props;
+    const { headerType } = props;
     const classes = useStyles();
     return (
         <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title}>{title}</Typography>
-                    <Button useCase={'main'}>{buttonTitle}</Button>
-                </Toolbar>
-            </AppBar>
+            <ThemeProvider theme={theme}>
+                <AppBar position="static"
+                        color={headerType == 'main' ? 'primary' : 'secondary'}>
+                    <Toolbar>
+                        <Typography variant="h6"
+                                    className={classes.title}
+                        >{returnHeaderTitle(headerType)}</Typography>
+                        <Button useCase={'main'}>{returnButtonTitle(headerType)}</Button>
+                    </Toolbar>
+                </AppBar>
+            </ThemeProvider>
         </div>
     );
 }
