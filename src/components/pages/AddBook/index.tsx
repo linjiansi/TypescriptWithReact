3edc433ebb1b849d77d25be4559cd16686f96cc6
khayashi-cq <k.hayashi@caraquri.com>
2021-05-14 19:12:30 +0900

    Modify edit and add book page component to fix paper component

--- src/components/pages/AddBook/index.tsx
+++ src/components/pages/AddBook/index.tsx
@@ -3,6 +3,7 @@ import Header from '../../molecules/Header/index';
 import BookScreen from '../../organisms/BookScreen/index';
 import Paper from '@material-ui/core/Paper';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import styled from 'styled-components';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -12,6 +13,11 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
+const PaperContainer = styled.div`
+    width: 800px;
+    margin: 0 auto;
+    background-color: green;
+`;
 
 export default function AddBook() {
     const classes = useStyles();
@@ -19,9 +25,11 @@ export default function AddBook() {
     return (
         <>
             <Header headerType={'main'}></Header>
-            <Paper elevation={8} className={classes.paper}>
-                <BookScreen bookMode='add'></BookScreen>
-            </Paper>
+            <PaperContainer>
+                <Paper elevation={8} className={classes.paper}>
+                    <BookScreen bookMode='add'></BookScreen>
+                </Paper>
+            </PaperContainer>
         </>
     );
 }
--- src/components/pages/EditBook/index.tsx
+++ src/components/pages/EditBook/index.tsx
@@ -3,6 +3,7 @@ import Header from '../../molecules/Header/index';
 import BookScreen from '../../organisms/BookScreen/index';
 import Paper from '@material-ui/core/Paper';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import styled from 'styled-components';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -12,16 +13,23 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
+const PaperContainer = styled.div`
+    width: 800px;
+    margin: 0 auto;
+    background-color: green;
+`;
 
 export default function EditBook() {
     const classes = useStyles();
 
     return (
-        <>
+        <>  
             <Header headerType={'main'}></Header>
-            <Paper elevation={8} className={classes.paper}>
-                <BookScreen bookMode='edit'></BookScreen>
-            </Paper>
+            <PaperContainer>
+                <Paper elevation={8} className={classes.paper}>
+                    <BookScreen bookMode='edit'></BookScreen>
+                </Paper>
+            </PaperContainer>
         </>
     );
 }
