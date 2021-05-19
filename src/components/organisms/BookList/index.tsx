133a78995bad9b7aec2d9f025293ed6be6d4d288
linjiansi <pmon.ken.1467@gmail.com>
2021-05-19 15:31:11 +0900

    Add comment to make easy to review

--- src/components/organisms/BookList/index.tsx
+++ src/components/organisms/BookList/index.tsx
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Header from '../../molecules/Header/index';
 import BookCard from '../../molecules/BookCard/index';
 
+//UIでのみ使用
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function BookList() {
