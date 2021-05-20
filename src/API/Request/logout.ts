de8db868cbef7d33141ce906d0db0a7865dc0a71
khayashi-cq <k.hayashi@caraquri.com>
2021-05-20 17:57:34 +0900

    Configure logout request

---
+++ src/API/Request/logout.ts
@@ -0,0 +1,17 @@
+import { KyHeadersInit } from 'ky/distribution/types/options';
+import { Request, configureApiClient } from '../Common/apiClient';
+
+const PATH = '/logout';
+const HTTP_METHOD = 'delete';
+const TOKEN = localStorage.getItem('token');
+const HEADER: KyHeadersInit = { token: `${TOKEN}` };
+
+export const logout = () => {
+  const request: Request = {
+    path: PATH,
+    httpMethod: HTTP_METHOD,
+    headers: HEADER,
+  };
+
+  return configureApiClient(request);
+}
