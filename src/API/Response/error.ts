28260af869b4779689b5f664f96c29f6d89b3edc
khayashi-cq <k.hayashi@caraquri.com>
2021-05-21 17:15:09 +0900

    Add type checker for error model

--- src/API/Response/error.ts
+++ src/API/Response/error.ts
@@ -4,3 +4,15 @@ export interface Error extends ResponseModel {
     status: number,
     massage: string[]
 }
+
+export const isError = (arg: unknown): arg is Error => {
+    const model = arg as Error;
+
+    return (
+        typeof model.status === 'number' &&
+        isMessage(model.massage)
+    );
+};
+
+const isMessage = (arg: string[]) =>
+    arg.some((arg) => typeof arg === 'string');
