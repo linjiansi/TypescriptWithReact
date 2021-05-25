cc16e3b72a7400b48bcee270da119b3adb2cedf8
khayashi-cq <k.hayashi@caraquri.com>
2021-05-25 18:52:24 +0900

    Add text-field snapshot test

---
+++ src/components/atoms/TextField/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { TextFieldComponent } from './index';
+
+it('renders correctly at TextFiledComponent', () => {
+  const tree = renderer
+    .create(<TextFieldComponent textFieldType={'text'}></TextFieldComponent>)
+    .toJSON();
+  expect(tree).toMatchSnapshot()
+});
