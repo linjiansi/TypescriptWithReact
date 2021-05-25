efcbd1ef0e1696c0804ed8c5e6659f38b7cde60e
linjiansi <pmon.ken.1467@gmail.com>
2021-05-25 17:40:08 +0900

    Add MemoryRouter to fix storybook error

--- src/components/molecules/BookCard/index.tsx
+++ src/components/molecules/BookCard/index.tsx
@@ -3,7 +3,7 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
+import { Link, MemoryRouter } from 'react-router-dom';
 import { BookPriceAndDateContainer, useStyles } from './style';
 
 type Props = {
@@ -17,27 +17,29 @@ export function BookCardComponent(props: Props) {
   const classes = useStyles();
 
   return (
-    <Link to={'/edit'}>
-      <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image="https://source.unsplash.com/random"
-          title="book"
-        />
-        <CardContent>
-          <Typography variant="body2" color="textPrimary">
-            書籍名: {bookName}
-          </Typography>
-          <BookPriceAndDateContainer>
-            <Typography variant="body2" color="textSecondary">
-              金額: {bookPrice ?? 0}
+    <MemoryRouter>
+      <Link to={'/edit'}>
+        <Card className={classes.root}>
+          <CardMedia
+            className={classes.media}
+            image="https://source.unsplash.com/random"
+            title="book"
+            />
+          <CardContent>
+            <Typography variant="body2" color="textPrimary">
+              書籍名: {bookName}
             </Typography>
-            <Typography variant="body2" color="textSecondary">
-              購入日: {purchaseDate ?? ''}
-            </Typography>
-          </BookPriceAndDateContainer>
-        </CardContent>
-      </Card>
-    </Link>
+            <BookPriceAndDateContainer>
+              <Typography variant="body2" color="textSecondary">
+                金額: {bookPrice ?? 0}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                購入日: {purchaseDate ?? ''}
+              </Typography>
+            </BookPriceAndDateContainer>
+          </CardContent>
+        </Card>
+      </Link>
+    </MemoryRouter>
   );
 }
