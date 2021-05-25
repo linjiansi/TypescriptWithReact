289ce6670cb43480a1fbe04d2bdd67a2cfc176cc
khayashi-cq <k.hayashi@caraquri.com>
2021-05-25 18:39:18 +0900

    Modify button component snapshot test

--- src/components/atoms/Button/index.test.js
+++ src/components/atoms/Button/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { ButtonComponent } from './index';
 
-it('renders correctly', () => {
+it('renders correctly at ButtonComponent', () => {
   const tree = renderer
     .create(<ButtonComponent useCase={'auth'}>テスト</ButtonComponent>)
     .toJSON();
