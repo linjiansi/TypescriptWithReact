4945bca717300a2d59d093c454191b160fe2e178
khayashi-cq <k.hayashi@caraquri.com>
2021-05-26 18:39:57 +0900

    Modify button component

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -2,26 +2,32 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { theme, useStyles } from './style';
+import classNames from 'classnames';
 
 type Props = {
   useCase: UseCase;
+  buttonType?: "submit";
+  form?: string;
   children: React.ReactNode;
 };
 
 export type UseCase = 'auth' | 'main';
 
 export function ButtonComponent(props: Props) {
-  const { useCase, children } = props;
+  const { useCase, buttonType, form, children } = props;
   const classes = useStyles();
+  const buttonClass = classNames(classes.button);
 
   return (
     <div className={useCase == 'auth' ? classes.auth : classes.main}>
       <ThemeProvider theme={theme}>
         <Button
-          className={classes.button}
+          className={buttonClass}
           variant="contained"
           color={useCase == 'auth' ? 'primary' : 'secondary'}
           fullWidth={useCase == 'auth'}
+          type={buttonType}
+          form={form}
         >
           {children}
         </Button>
