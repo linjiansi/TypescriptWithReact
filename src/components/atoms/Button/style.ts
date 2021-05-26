397dc8528bc8d819bad0486e16ea3f83f1a53532
linjiansi <pmon.ken.1467@gmail.com>
2021-05-26 18:39:57 +0900

    Modify button component

--- src/components/atoms/Button/style.ts
+++ src/components/atoms/Button/style.ts
@@ -11,8 +11,7 @@ export const useStyles = makeStyles((theme: Theme) =>
       width: 100,
     },
     button: {
-      marginTop: theme.spacing(1),
-      marginBottom: theme.spacing(1),
+      margin: theme.spacing(1),
     },
   })
 );
