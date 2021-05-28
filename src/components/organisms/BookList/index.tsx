39c497c6a36249280927409b510ab353dfa54e3f
linjiansi <pmon.ken.1467@gmail.com>
2021-05-28 15:56:31 +0900

    Add id parameter to edit url

--- src/components/organisms/BookList/index.tsx
+++ src/components/organisms/BookList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { HeaderComponent } from '../../molecules/Header/index';
 import { BookCardComponent } from '../../molecules/BookCard/index';
+import { Link } from 'react-router-dom';
 
 // TODO UIでのみ使用
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -11,9 +12,9 @@ export function BookListComponent() {
     <>
       <HeaderComponent headerType={'bookList'}></HeaderComponent>
       <Grid container spacing={4}>
-        {cards.map((card) => (
+        {cards.map((card, index) => (
           <Grid item key={card} xs={12} sm={6} md={4}>
-            <BookCardComponent bookName={'hogehoge'}></BookCardComponent>
+            <Link to={`/edit/${index}`}><BookCardComponent bookName={'hogehoge'}></BookCardComponent></Link>
           </Grid>
         ))}
       </Grid>
