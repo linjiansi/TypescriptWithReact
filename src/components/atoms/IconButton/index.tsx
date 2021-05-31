57b334c7beeb828b2f545cac6a5a71d7843e9570
linjiansi <pmon.ken.1467@gmail.com>
2021-05-31 12:14:58 +0900

    Add icon button component

---
+++ src/components/atoms/IconButton/index.tsx
@@ -0,0 +1,37 @@
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { IconButton, InputAdornment } from '@material-ui/core';
+import VisilityIcon from '@material-ui/icons/Visibility';
+import VisilityIconOff from '@material-ui/icons/VisibilityOff';
+
+type Props = {
+  setShowPassword?: Dispatch<SetStateAction<boolean>>;
+};
+
+export function IconButtonComponent(props: Props) {
+  const { setShowPassword } = props
+  const [showIcon, setShowIcon] = useState<boolean>(true);
+
+  const handleClickShowPassword = () => {
+    setShowIcon(!showIcon);
+    if (setShowPassword) {
+      setShowPassword(showIcon);
+    }
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  <InputAdornment position="end">
+    <IconButton
+      aria-label="toggle password visibility"
+      onClick={handleClickShowPassword}
+      onMouseDown={handleMouseDownPassword}
+      edge="end"
+    >
+      {showIcon? <VisilityIcon /> : <VisilityIconOff />}
+    </IconButton>
+  </InputAdornment>
+}
