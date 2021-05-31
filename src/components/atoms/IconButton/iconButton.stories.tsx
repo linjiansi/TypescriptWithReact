411e6b847fc89c0a9d2c27b53e0b3ca136e4a2ba
khayashi-cq <k.hayashi@caraquri.com>
2021-05-31 12:14:58 +0900

    Add icon button component

---
+++ src/components/atoms/IconButton/iconButton.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IconButtonComponent } from './index';
+
+export default {
+  title: 'IconButton',
+  component: IconButtonComponent,
+};
+
+export function story() {
+  return <IconButtonComponent></IconButtonComponent>
+}
